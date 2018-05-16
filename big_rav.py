cbb2c87750a1c68cfb89bd61c055f9f69f32d13b
ctpelnar1988 <christopherpelnar@gmail.com>
2018-05-16 05:39:47 +0000

    Adding autonomous mode settings with PWM control.

--- big_rav.py
+++ big_rav.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import time
 from time import sleep
 import pygame
+import serial
 
 pygame.init()
 pygame.joystick.init()
@@ -11,18 +12,17 @@ j.init()
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-AUTONOMOUS_MODE = False
-
-### SENSORS
-GPIO_TRIGGER = 4
-
-GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
-
-ECHOS = {"ECHO_LEFT": 17, "ECHO_MIDDLE": 27, "ECHO_RIGHT": 22}
-for e in ECHOS:
-    GPIO.setup(ECHOS[e], GPIO.IN)
+# Autonomous or Controller:
+a = input("Autonomous Mode? T/F: ")
+if a == "T":
+    AUTONOMOUS_MODE = True
+else:
+    AUTONOMOUS_MODE = False
 
 ### MOTORS
+FREQUENCY = 20
+DUTY_CYCLE = 75
+PWM_STOP = 0
 
 ## FRONT RIGHT
 Motor_FrontR_1 = 7
@@ -60,30 +60,26 @@ GPIO.setup(Motor_BackL_1, GPIO.OUT)
 GPIO.setup(Motor_BackL_2, GPIO.OUT)
 GPIO.setup(Motor_BackL_Enable, GPIO.OUT)
 
-### SENSOR METHODS
-def distance(e):
-    print(e)
-    GPIO.output(GPIO_TRIGGER, True)
-    time.sleep(0.00001)
-    GPIO.output(GPIO_TRIGGER, False)
-    StartTime = time.time()
-    StopTime = time.time()
-    while GPIO.input(e) == 0:
-        StartTime = time.time()
-    while GPIO.input(e) == 1:
-        StopTime = time.time()
+# Set up PWM pins:
+pwmMotor_FrontR_1 = GPIO.PWM(Motor_FrontR_1, FREQUENCY)
+pwmMotor_FrontR_2 = GPIO.PWM(Motor_FrontR_2, FREQUENCY)
+pwmMotor_BackR_3 = GPIO.PWM(Motor_BackR_3, FREQUENCY)
+pwmMotor_BackR_4 = GPIO.PWM(Motor_BackR_4, FREQUENCY)
+pwmMotor_FrontL_3 = GPIO.PWM(Motor_FrontL_3, FREQUENCY)
+pwmMotor_FrontL_4 = GPIO.PWM(Motor_FrontL_4, FREQUENCY)
+pwmMotor_BackL_1 = GPIO.PWM(Motor_BackL_1, FREQUENCY)
+pwmMotor_BackL_2 = GPIO.PWM(Motor_BackL_2, FREQUENCY)
 
-    TimeElapsed = StopTime - StartTime
-    dist = (TimeElapsed * 34300) / 2
-    return dist
+# Initial PWM Start:
+pwmMotor_FrontR_1.start(PWM_STOP)
+pwmMotor_FrontR_2.start(PWM_STOP)
+pwmMotor_BackR_3.start(PWM_STOP)
+pwmMotor_BackR_4.start(PWM_STOP)
+pwmMotor_FrontL_3.start(PWM_STOP)
+pwmMotor_FrontL_4.start(PWM_STOP)
+pwmMotor_BackL_1.start(PWM_STOP)
+pwmMotor_BackL_2.start(PWM_STOP)
 
-def run_distance():
-    sensors = []
-    for e in ECHOS:
-        dist = distance(ECHOS[e])
-        sensors.append({"{}".format(e): "{}".format(dist)})
-        time.sleep(1)
-    return sensors
 
 def toggle_autonomous_mode(AUTONOMOUS_MODE):
     if AUTONOMOUS_MODE == True:
@@ -137,14 +133,14 @@ def forward():
 
 def right():
     # Front Right Motors
-    GPIO.output(Motor_FrontR_1, GPIO.HIGH)
-    GPIO.output(Motor_FrontR_2, GPIO.LOW)
-    GPIO.output(Motor_FrontR_Enable, GPIO.HIGH)
+    #GPIO.output(Motor_FrontR_1, GPIO.HIGH)
+    #GPIO.output(Motor_FrontR_2, GPIO.LOW)
+    #GPIO.output(Motor_FrontR_Enable, GPIO.HIGH)
 
     # Back Right Motors
-    GPIO.output(Motor_BackR_3, GPIO.HIGH)
-    GPIO.output(Motor_BackR_4, GPIO.LOW)
-    GPIO.output(Motor_BackR_Enable, GPIO.HIGH)
+    #GPIO.output(Motor_BackR_3, GPIO.HIGH)
+    #GPIO.output(Motor_BackR_4, GPIO.LOW)
+    #GPIO.output(Motor_BackR_Enable, GPIO.HIGH)
 
     # Front Left Motors
     GPIO.output(Motor_FrontL_3, GPIO.HIGH)
@@ -168,14 +164,14 @@ def left():
     GPIO.output(Motor_BackR_Enable, GPIO.HIGH)
 
     # Front Left Motors
-    GPIO.output(Motor_FrontL_3, GPIO.LOW)
-    GPIO.output(Motor_FrontL_4, GPIO.HIGH)
-    GPIO.output(Motor_FrontL_Enable, GPIO.HIGH)
+    #GPIO.output(Motor_FrontL_3, GPIO.LOW)
+    #GPIO.output(Motor_FrontL_4, GPIO.HIGH)
+    #GPIO.output(Motor_FrontL_Enable, GPIO.HIGH)
 
     # Back Left Motors
-    GPIO.output(Motor_BackL_1, GPIO.LOW)
-    GPIO.output(Motor_BackL_2, GPIO.HIGH)
-    GPIO.output(Motor_BackL_Enable, GPIO.HIGH)
+    #GPIO.output(Motor_BackL_1, GPIO.LOW)
+    #GPIO.output(Motor_BackL_2, GPIO.HIGH)
+    #GPIO.output(Motor_BackL_Enable, GPIO.HIGH)
 
 def stop():
     GPIO.output(Motor_FrontR_Enable, GPIO.LOW)
@@ -183,17 +179,124 @@ def stop():
     GPIO.output(Motor_FrontL_Enable, GPIO.LOW)
     GPIO.output(Motor_BackL_Enable, GPIO.LOW)
     
-def drive_autonomously():
-    print(run_distance())
+# PWM Control:
+
+def pwm_stop():
+    pwmMotor_FrontR_1.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_FrontR_2.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_BackR_3.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_BackR_4.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_FrontL_3.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_FrontL_4.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_BackL_1.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_BackL_2.ChangeDutyCycle(PWM_STOP)
     
-              
+def pwm_forward():
+    # Front Right Motors
+    pwmMotor_FrontR_1.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_FrontR_2.ChangeDutyCycle(DUTY_CYCLE)
+    GPIO.output(Motor_FrontR_Enable, GPIO.HIGH)
+
+    # Back Right Motors
+    pwmMotor_BackR_3.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_BackR_4.ChangeDutyCycle(DUTY_CYCLE)
+    GPIO.output(Motor_BackR_Enable, GPIO.HIGH)
+
+    # Front Left Motors
+    pwmMotor_FrontL_3.ChangeDutyCycle(DUTY_CYCLE)
+    pwmMotor_FrontL_4.ChangeDutyCycle(PWM_STOP)
+    GPIO.output(Motor_FrontL_Enable, GPIO.HIGH)
+
+    # Back Left Motors
+    pwmMotor_BackL_1.ChangeDutyCycle(DUTY_CYCLE)
+    pwmMotor_BackL_2.ChangeDutyCycle(PWM_STOP)
+    GPIO.output(Motor_BackL_Enable, GPIO.HIGH)
+    
+def pwm_backward():
+    # Front Right Motors
+    pwmMotor_FrontR_1.ChangeDutyCycle(DUTY_CYCLE)
+    pwmMotor_FrontR_2.ChangeDutyCycle(PWM_STOP)
+    GPIO.output(Motor_FrontR_Enable, GPIO.HIGH)
+
+    # Back Right Motors
+    pwmMotor_BackR_3.ChangeDutyCycle(DUTY_CYCLE)
+    pwmMotor_BackR_4.ChangeDutyCycle(PWM_STOP)
+    GPIO.output(Motor_BackR_Enable, GPIO.HIGH)
+
+    # Front Left Motors
+    pwmMotor_FrontL_3.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_FrontL_4.ChangeDutyCycle(DUTY_CYCLE)
+    GPIO.output(Motor_FrontL_Enable, GPIO.HIGH)
+
+    # Back Left Motors
+    pwmMotor_BackL_1.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_BackL_2.ChangeDutyCycle(DUTY_CYCLE)
+    GPIO.output(Motor_BackL_Enable, GPIO.HIGH)
+    
+def pwm_right():
+    # Front Left Motors
+    pwmMotor_FrontL_3.ChangeDutyCycle(DUTY_CYCLE)
+    pwmMotor_FrontL_4.ChangeDutyCycle(PWM_STOP)
+    GPIO.output(Motor_FrontL_Enable, GPIO.HIGH)
+
+    # Back Left Motors
+    pwmMotor_BackL_1.ChangeDutyCycle(DUTY_CYCLE)
+    pwmMotor_BackL_2.ChangeDutyCycle(PWM_STOP)
+    GPIO.output(Motor_BackL_Enable, GPIO.HIGH)
+    
+def pwm_left():
+    # Front Right Motors
+    pwmMotor_FrontR_1.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_FrontR_2.ChangeDutyCycle(DUTY_CYCLE)
+    GPIO.output(Motor_FrontR_Enable, GPIO.HIGH)
+
+    # Back Right Motors
+    pwmMotor_BackR_3.ChangeDutyCycle(PWM_STOP)
+    pwmMotor_BackR_4.ChangeDutyCycle(DUTY_CYCLE)
+    GPIO.output(Motor_BackR_Enable, GPIO.HIGH)
+    
+def drive_autonomously():
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    s = ser.readline()
+    print(s)
+    if b'1\r\n' == s:
+        print("FORWARD!")
+        pwm_forward()
+    elif b'2\r\n' == s:
+        pwm_stop()
+        time.sleep(2)
+        pwm_right()
+        time.sleep(1)
+        print("RIGHT!")
+        pwm_stop()
+        #time.sleep(2)
+    elif b'3\r\n' == s:
+        pwm_stop()
+        time.sleep(2)
+        pwm_backward()
+        time.sleep(1)
+        print("BACK!")
+        pwm_stop()
+        time.sleep(2)
+    elif b'4\r\n' == s:
+        pwm_stop()
+        time.sleep(2)
+        pwm_left()
+        time.sleep(1)
+        print("LEFT!")
+        pwm_stop()
+        time.sleep(2)
+    else:
+        print(s)
 
 if __name__ == '__main__':
     try:
         while True:
             if AUTONOMOUS_MODE == True:
                 drive_autonomously()
+                #print("AUTONOMOUS_MODE")
             else:
+                #print("DRIVER_MODE")
                 events = pygame.event.get()
                 for event in events:
                     if event.type == pygame.JOYBUTTONDOWN:
@@ -214,5 +317,6 @@ if __name__ == '__main__':
                         print("Listening...")
     except KeyboardInterrupt:
         print("Shutting Down and Cleaning Up")
+        stop()
         GPIO.cleanup()
 
